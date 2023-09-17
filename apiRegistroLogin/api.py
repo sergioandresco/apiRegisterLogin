b2e36e8eac09e655ff36d50dc673bf9838a3b340
sergioandresco <sergiocobos6@outlook.com>
2023-09-16 19:54:15 -0500

    Create class UserSerializer and two functions for create and validate user, aditional create api.py file

---
+++ apiRegistroLogin/api.py
@@ -0,0 +1,3 @@
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
--- apiRegistroLogin/serializers.py
+++ apiRegistroLogin/serializers.py
@@ -1,5 +1,27 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class UserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
     
+    def create(self, validated_data):
+        instance = User()
+        instance.first_name = validated_data.get('first_name')
+        instance.last_name = validated_data.get('last_name')
+        instance.username = validated_data.get('username')
+        instance.email = validated_data.get('email')
+        instance.set_password(validated_data.get('password'))
+        instance.save()
+        return instance
+    
+    def validate_username(self, data):
+        users = User.objects.filter(username = data)
+        if len(users) != 0:
+            raise serializers.ValidationError("Este nombre de usuario ya existe, ingrese uno nuevo")
+        else:
+            return data
